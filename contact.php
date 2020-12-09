1003737278b5f28b3318101089a71bbce0ed46f7
RituCs <rituparnocs@gmail.com>
2020-12-09 14:58:12 +0530

    Change frontend and add social media link

---
+++ contact.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Secret Talk</title>
+    <meta name="description" content="Welcome to the best secret talk app">
+    <meta name="keywords"  content="secret,question,answer,message">
+    <link rel="stylesheet" href="css/contact.css">
+    <link rel="stylesheet" media="screen and (max-width:768px)" href="css/mobile.css">
+    <link rel="stylesheet" media="screen and (min-width:1100px)" href="widescreen.css">
+    <script src="https://kit.fontawesome.com/b06791e85d.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav id="navbar">
+      <h1 class="logo"><a href=project.html>ST</a></h1>
+      <ul>
+        <li><a href="signup.php">Sign Up</a></li>
+        <li><a href="login.php">Sign In</a></li>
+        <li><a href="contact.php">Contact</a></li> 
+      </ul>
+    </nav>
+    <div class="container">
+     <div class="box">
+        <div class="content-box">
+           <h2>CONTACT US</h2><br>
+           <p>snpdn1997@gmail<font face="Symbol">.</font>com</p>
+           <a>rituparnocs@gmail<font face="Symbol">.</font>com</p><br>
+           <img
+           src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/underline_welcome.svg"
+         />
+        </div>
+     </div>
+   </div>
+   <footer id="main-footer">
+     
+     <div class="footer">
+       <a href="#" id="pp">Privacy Policy</a>
+   
+       <a href="#" id="tt">Terms of Use</a>
+     </div>
+       
+     
+   
+ </footer>
+  </body>
+</html>
